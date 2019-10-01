0c2435cc89f537e0cb4619b4d5b92301c44cde92
rudij7 <joshipurarutash@gmail.com>
2019-10-01 18:08:25 +0530

    APP: Add login and registration integration with Firebase

--- app/src/main/java/com/example/tablayout/Login.java
+++ app/src/main/java/com/example/tablayout/Login.java
@@ -1,29 +1,48 @@
 package com.example.tablayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText EmailField;
+    private EditText PasswordField;
+
     Button b,r;
-    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        b=findViewById(R.id.logbtn);
-        r=findViewById(R.id.regbtn);
+        mAuth = FirebaseAuth.getInstance();
+        onStart();
+        EmailField = findViewById(R.id.email);
+        PasswordField = findViewById(R.id.pass);
+        b = findViewById(R.id.logbtn);
+        r = findViewById(R.id.regbtn);
         r.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext() ,Register.class);
+                Intent i = new Intent(getApplicationContext(), Register.class);
                 startActivity(i);
             }
         });
@@ -31,9 +50,73 @@ public class Login extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext() ,MainActivity.class);
-                startActivity(i);
+                signIn(EmailField.getText().toString(), PasswordField.getText().toString());
+
             }
         });
     }
-}
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            // Check if user is signed in (non-null) and update UI accordingly.
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if(currentUser!=null) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        }
+
+    private void signIn(String email, String password) {
+        if (!validateForm()) {
+            return;
+        }
+
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+
+                        }
+
+                        // [START_EXCLUDE]
+
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = EmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            EmailField.setError("Required.");
+            valid = false;
+        } else {
+            EmailField.setError(null);
+        }
+
+        String password = PasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            PasswordField.setError("Required.");
+            valid = false;
+        } else {
+            PasswordField.setError(null);
+        }
+
+        return valid;
+    }
+    }
+
